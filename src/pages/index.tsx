443af53268e29b74170a7eca1cc3dca3d02896d2
Leenus Darnal <leenus.darnal@gmail.com>
2023-04-10 17:17:38 +0530

    new files and route for profle and post/id

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,6 +10,7 @@ import Image from "next/image"
 import { Loading, LoadingPage } from "../components/Loading"
 import { useState } from "react"
 import { toast } from "react-hot-toast"
+import Link from "next/link"
 
 dayjs.extend(relativeTIme)
 
@@ -91,10 +92,14 @@ const PostView = (props: PostWithUser) => {
       />
       <div className="flex flex-col">
         <div className="flex gap-1  text-slate-300">
-          <span>{`@${author.username} `}</span>{" "}
-          <span className="font-thin">{`· ${dayjs(
-            post.createdAt
-          ).fromNow()}`}</span>
+          <Link href={`/@${author.username}`}>
+            <span>{`@${author.username} `}</span>
+          </Link>
+          <Link href={`/post/${post.id}`}>
+            <span className="font-thin">{`· ${dayjs(
+              post.createdAt
+            ).fromNow()}`}</span>
+          </Link>
         </div>
         <span className="text-2xl">{post.content}</span>
       </div>
